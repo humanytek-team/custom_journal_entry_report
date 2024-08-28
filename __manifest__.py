a28eda057aaff9f71a790289c873fdc9771f50fd
mbrehumanytek <matias@humanytek.com>
2024-08-28 17:54:15 +0000

    ajustes manifest rebuild

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,5 @@
-# pylint: disable=missing-module-docstring,pointless-statement
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    ###########################
-    # Delete all the commented lines after editing the module
-    ###########################
     "name": "Custom Journal Entry Report",
     "summary": """
         Custom Journal Entry Report allows you to print the journal entries from account.move that are not invoices.""",
@@ -12,7 +8,7 @@
     "website": "https://humanitek.com/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
@@ -26,32 +22,4 @@
         "reports/journal_entry_report.xml",
         "reports/journal_entry_template.xml",
     ],
-    ###########################
-    # Delete all the commented lines after editing the module
-    ###########################
-    ### XML Data files
-    # "data": [
-    #     "security/ir.model.access.csv",
-    #     "views/views.xml",
-    #     "views/templates.xml",
-    # ],
-    ### XML Demo files
-    # only loaded in demo mode
-    # "demo": [
-    #     "demo/demo.xml",
-    # ],
-    ### Assets
-    # In 15.0, Odoo adds a new way to add js/css assets files to a module.
-    # https://www.holdenrehg.com/blog/2021-10-08_odoo-manifest-asset-bundles
-    # "assets": {
-    #     "web.assets_backend": [
-    #         "/my_module/path/to/file"
-    #     ],
-    #     "web.assets_qweb": [
-    #         "/my_module/path/to/file", # QWeb templates. Example: 'pos_sale/static/src/xml/**/*',
-    #     ],
-    # }
-    ###########################
-    # Delete all the commented lines after editing the module
-    ###########################
 }
